100789761f2659853667391e5b12f530da0314cb
Nil Portugués Calderó <nilopc@gmail.com>
2016-02-21 23:35:14 +0100

    Files

---
+++ docker.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+SHARED='./docker-compose.yml'
+
+case "$1" in
+    start)
+        docker-compose -f $SHARED start;
+        ;;
+    stop)
+        docker-compose -f $SHARED stop;
+        ;;
+    init)
+        docker-compose -f $SHARED  up --force-recreate -d;
+        ;;
+    destroy)
+        docker-compose -f $SHARED stop;
+        docker-compose -f $SHARED rm;
+        ;;
+    update)
+        docker-compose -f $SHARED stop;
+        docker-compose -f $SHARED build;
+        docker-compose -f $SHARED  up --force-recreate -d;
+        ;;
+    *)
+        echo "Options to use: {start|stop|init|destroy|update}"
+        exit 1
+        ;;
+esac
+
